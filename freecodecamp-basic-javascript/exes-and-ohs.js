7fc68184171de53dc4aa4d245b2caaa5305d95ee
aluxaf1 <97885017+aluxaf1@users.noreply.github.com>
2022-01-23 22:07:45 +0300

    Codewars thing

--- freecodecamp-basic-javascript/exes-and-ohs.js
+++
@@ -1,8 +0,0 @@
-function XO(str) {
-    var sum = 0;
-    for(var i=0; i<str.length; i++){
-      if(str[i].toLowerCase() == 'x') sum++;
-      if(str[i].toLowerCase() == 'o') sum--;
-    }
-    return sum == 0;
-  }
